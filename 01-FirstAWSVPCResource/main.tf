dc0b3ed622cacb9f5e98cc379c0fbcb7f0b234a3
Ashish Shah <ashishatcsc@gmail.com>
2023-07-26 18:50:09 +0530

    We can have provider detain in provider file and so split the same from moving the same from main.tf file.

--- 01-FirstAWSVPCResource/main.tf
+++ 01-FirstAWSVPCResource/main.tf
@@ -1,8 +1,4 @@
-provider "aws" {
-  #access_key = "your_access_key"
-  #secret_key = "your_secret_key"
-  region     = "eu-west-2"
-}
+
 
 resource "aws_vpc" "myfirstvpc" {
   cidr_block = "10.0.0.0/16"
